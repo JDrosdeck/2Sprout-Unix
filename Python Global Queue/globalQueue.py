8f653cd2f2591d690099d6dffede14c1066fa1ca
Jonathan Drosdeck <Jdrosdeck@gmail.com>
2009-03-08 00:40:52 -0500

    Re-wrote the global queue using parallel python

--- Python Global Queue/globalQueue.py
+++ Python Global Queue/globalQueue.py
@@ -2,8 +2,8 @@
 import Queue
 import threading
 import urllib2
-import time, os
-from BeautifulSoup import BeautifulSoup
+import time, os, sys, pp
+import BeautifulSoup 
 
 hosts = ["http://yahoo.com", "http://google.com", "http://amazon.com",
         "http://ibm.com", "http://apple.com"]
@@ -31,28 +31,45 @@ class ThreadUrl(threading.Thread):
     """Threaded Url Grab"""
 
     def run(self):
-        while 1:
+		ppservers = ()
+		ncpus = 3
+		job_server = pp.Server(ncpus, ppservers=ppservers)
+		while 1:
 			time.sleep(.01)
 			if queue.empty() == False:
 				host = queue.get()
-				url = urllib2.urlopen(host)
-				chunk = url.read()
+				job1 = job_server.submit(getSiteHTML, (host,),(),("urllib2",))
+				result = job1()
+				print "recieved: ", result
 				#place chunk into out queue
-				out_queue.put(chunk)
+				out_queue.put(result)
 
-     
+def getSiteHTML(n):
+	url = urllib2.urlopen(n)
+	chunk = url.read()
+	return chunk
+
+def mineHTML(n):
+	soup = BeautifulSoup.BeautifulSoup(n)
+	title = soup.findAll(['title'])
+	print title
+	return title
 
 class DatamineThread(threading.Thread):
     """Threaded Url Grab"""
     def run(self):
-        while 1:
+		ppservers = ()
+		ncpus = 3
+		job_server = pp.Server(ncpus, ppservers=ppservers)
+		while 1:
 			time.sleep(.01)
 			if out_queue.empty() == False:
 				#grabs host from queue
 				chunk = out_queue.get()
-				#parse the chunk
-				soup = BeautifulSoup(chunk)
-				print soup.findAll(['title'])
+				job2 = job_server.submit(mineHTML, (chunk,),(),("BeautifulSoup",))
+				result2 = job2()
+				job_server.print_stats()
+				print result2
 
 
 
@@ -67,25 +84,13 @@ def main():
 	readFromPipe().start()
 
 
+	
+	ThreadUrl().start()
 
-	
-	ThreadUrl().start()
-	ThreadUrl().start()
-	ThreadUrl().start()
-	ThreadUrl().start()
-	ThreadUrl().start()
-	ThreadUrl().start()
-	
-	
 #	t.start()
-					
-    			
+					 			
 	DatamineThread().start()
-	DatamineThread().start()
-	DatamineThread().start()
-	DatamineThread().start()
-	DatamineThread().start()
-	DatamineThread().start()
+
 	
 	
 #	dt.start()
--- pysock.py
+++ pysock.py
@@ -6,15 +6,22 @@ port = 4950
 host = socket.gethostbyname(hostname)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	
 
-for x in xrange(50):
+for x in xrange(10):
 	m = md5.new()
-	tempMessage = "what^120510^" + str(x) + "^<sproutCast><title> on your facedump oyouryouryouyouryyouryouryouryoyouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryoururyouryouryouryouryouryouryouryouryouryouryouryouryourouryouryoyouryouryouryouryouryouryouryouryouryoururyouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryourn your facedump on your facedump on your facedump on your facedump on your facedump on your facedump on your facen your facedump on your faceyouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryouryour</title></sproutCast>"
+	tempMessage = "what^120510^" + str(x) + "^	<?xml version=\"1.0\" encoding=\"UTF-8\"?><page id=\"122\" update=\"0\"><url>http://lifehacker.com/5059400/extractnow-is-a-lightning-fast-bulk-extraction-tool</url><title>Featured Windows Download: ExtractNow Is a Lightning Fast Bulk Extraction Tool</title><timestamp>1223456554</timestamp></page>"
 	m.update(tempMessage);
 	msg = str(m.hexdigest()) + "^" + tempMessage
 	print msg
 	time.sleep(.01)
 	s.sendto(msg,(host,port))
 
+m = md5.new()
+tempMessage = "what^120510^" + str(20) + "^	<?xml version=\"1.0\" encoding=\"UTF-8\"?><page id=\"122\" update=\"0\"><url>http://lifehacker.com/5059400/extractnow-is-a-lightning-fast-bulk-extraction-tool</url><title>Featured Windows Download: ExtractNow Is a Lightning Fast Bulk Extraction Tool</title><timestamp>1223456554</timestamp></page>"
+m.update(tempMessage);
+msg = str(m.hexdigest()) + "^" + tempMessage
+print msg
+time.sleep(.01)
+s.sendto(msg,(host,port))
 
 
 
